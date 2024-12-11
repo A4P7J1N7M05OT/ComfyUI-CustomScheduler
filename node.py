c0298878b0829cc32140f732cfeee38257989d52
6f4T7y9Q <127436319+A4P7J1N7M05OT@users.noreply.github.com>
2024-12-11 03:59:32 +0100

    remove more limits

--- node.py
+++ node.py
@@ -5,13 +5,13 @@ class CustomScheduler:
     def INPUT_TYPES(s):
         inputs = {
             "required": {                
-                "steps": ("INT", {"default": 4, "min": 1, "max": 25}),
+                "steps": ("INT", {"default": 4, "min": 1, "max": 1000}),
             },
             "optional": {},             
         }
 
         defaults = [4.12, 1.62, 0.7, 0.04]
-        for i in range(0, 26):
+        for i in range(0, 1000):
             default = defaults[i] if i < len(defaults) else 0.0
             inputs["optional"][f"sigma_{i}"] = ("FLOAT", {"default": default, "min": 0.0, "max": 999999.0, "step": 0.01, "round": 0.0001})
         
